ded6af810551d9666f47b4acb556b4fcea47df53
Thiago Rangel <108483500+Thiago-Rangel45@users.noreply.github.com>
2025-07-22 19:45:13 -0300

    Delete TrackingTools/TransientTrack directory

--- TrackingTools/TransientTrack/src/classes.h
+++
@@ -1,9 +0,0 @@
-#ifndef TrackingTools_TransientTrack_src_classes_h
-#define TrackingTools_TransientTrack_src_classes_h
-
-#include <vector>
-
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
-
-#endif
