8a44e524aca8f8c1129934c21d7cef7edb11f461
Thiago Rangel <108483500+Thiago-Rangel45@users.noreply.github.com>
2025-07-22 19:45:41 -0300

    Delete PhysicsTools directory

--- PhysicsTools/BPHNano/production/apply_crab_command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Get the command from the first argument
-COMMAND=$1
-# Find directories exactly two levels deep
-find . -mindepth 2 -maxdepth 2 -type d | while read -r dir; do
-    echo "Processing directory: $dir"
-    crab $COMMAND -d "$dir"
-done
