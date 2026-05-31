81c6ef6bf92b8724413574117e3f10ca08ef7d7c
Tiago de Andrade Rangel Monteiro <thiago_rangel45@hotmail.com>
2026-05-31 20:30:33 +0200

    Testing submission for 2023 dataset

--- Production/CRAB/crab_config_data.py
+++ Production/CRAB/crab_config_data.py
@@ -1,23 +1,23 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'BPH_NanoAOD_Data_Run2022C_2'
+config.General.requestName = 'BPH_NanoAOD_Data_Run20223'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '../BPH_Data.py'
+config.JobType.psetName = '/afs/cern.ch/user/t/tdeandra/CMSSW_15_1_0/src/BPH_Data_cfg.py'
 config.Data.unitsPerJob    = 40
 config.JobType.numCores = 4
 config.JobType.maxMemoryMB = 4000
 config.JobType.sendExternalFolder = True
 
-config.Data.inputDataset = '/ParkingDoubleMuonLowMass2/Run2022C-PromptReco-v1/MINIAOD'
+config.Data.inputDataset = '/ParkingDoubleMuonLowMass0/Run2023C-PromptReco-v4/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.publication = False
-config.Data.outputDatasetTag = 'BPH_NanoAOD_Run2022C'
-config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions22/Cert_Collisions2022_eraC_355862_357482_Golden.json'
+config.Data.outputDatasetTag = 'BPH_NanoAOD_Run2023C'
+config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions23/PromptReco/Cert_Collisions2023_eraC_367095_368823_Golden.json'
 
 config.Site.storageSite = 'T2_BR_UERJ'
